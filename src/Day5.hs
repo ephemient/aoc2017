c1a8ef92242293c0aa03e85387cd5edf2a49ca79
Daniel Lin <ephemient@gmail.com>
2017-12-05 01:41:17 -0500

    Day 5: Huge speedup by using mutable arrays

--- src/Day5.hs
+++ src/Day5.hs
@@ -2,25 +2,30 @@
 Module:         Day5
 Description:    <http://adventofcode.com/2017/day/5 Day 5: A Maze of Twisty Trampolines, All Alike>
 -}
-{-# LANGUAGE BangPatterns, PatternGuards, TupleSections #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day5 (day5a, day5b) where
 
 import Control.Monad (join)
+import Control.Monad.ST (ST, runST)
+import Data.Array.MArray (newListArray, readArray, writeArray)
+import Data.Array.ST (STUArray)
 import Data.Bool (bool)
+import Data.Ix (inRange)
 
 -- | At each step, the current item is used as a relative index shift.
 steps :: (Int -> Int) -- ^ After each step, mutate the previous item
       -> [Int]        -- ^ Start at index 0
       -> Int          -- ^ Number of steps until the index is out of bounds
-steps f = steps' 0 . ([],) where
-    steps' !k (before, a:as)
-      | a >= 0, (bs, after') <- splitAt a after
-      = steps' (k + 1) (reverse bs ++ before, after')
-      | a < 0, (as, a':before') <- splitAt (-a - 1) before
-      = steps' (k + 1) (before', a' : reverse as ++ after)
-      where after = f a : as
-    steps' k _ = k
+steps f jumps = runST $ do
+    let bounds = (0, length jumps - 1)
+    mem <- newListArray bounds jumps :: ST s (STUArray s Int Int)
+    let step ip k | inRange bounds ip = do
+            jump <- readArray mem ip
+            writeArray mem ip $ f jump
+            step (ip + jump) $! k + 1
+        step _ k = return k
+    step 0 0
 
 day5a :: String -> Int
 day5a = steps succ . map read . lines
