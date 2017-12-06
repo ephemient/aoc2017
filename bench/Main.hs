ce8b9c2bef1788a92b739fd41b479f625e87a8c8
Daniel Lin <ephemient@gmail.com>
2017-12-06 16:39:12 -0500

    Add criterion benchmarks

---
+++ bench/Main.hs
@@ -0,0 +1,44 @@
+module Main (main) where
+
+import Criterion.Main (bench, bgroup, defaultMain, env, nf)
+import Day1 (day1a, day1b)
+import Day2 (day2a, day2b)
+import Day3 (day3a, day3b)
+import Day4 (day4a, day4b)
+import Day5 (day5a, day5b)
+import Day6 (day6a, day6b)
+import Paths_aoc2017 (getDataFileName)
+
+getDayInput :: Int -> IO String
+getDayInput i = getDataFileName ("day" ++ show i ++ ".txt") >>= readFile
+
+readDayInput :: (Read a) => Int -> IO a
+readDayInput = fmap read . getDayInput
+
+main :: IO ()
+main = defaultMain
+  [ env (getDayInput 1) $ \input -> bgroup "Day 1"
+      [ bench "part 1" $ nf day1a input
+      , bench "part 2" $ nf day1b input
+      ]
+  , env (getDayInput 2) $ \input -> bgroup "Day 2"
+      [ bench "part 1" $ nf day2a input
+      , bench "part 2" $ nf day2b input
+      ]
+  , env (readDayInput 3) $ \input -> bgroup "Day 3"
+      [ bench "part 1" $ nf day3a input
+      , bench "part 2" $ nf day3b input
+      ]
+  , env (getDayInput 4) $ \input -> bgroup "Day 4"
+      [ bench "part 1" $ nf day4a input
+      , bench "part 2" $ nf day4b input
+      ]
+  , env (getDayInput 5) $ \input -> bgroup "Day 5"
+      [ bench "part 1" $ nf day5a input
+      , bench "part 2" $ nf day5b input
+      ]
+  , env (getDayInput 6) $ \input -> bgroup "Day 6"
+      [ bench "part 1" $ nf day6a input
+      , bench "part 2" $ nf day6b input
+      ]
+  ]
