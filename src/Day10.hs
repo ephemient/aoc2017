d2ec466b0320d8b6355ced81fc85e5162c2f85d8
Daniel Lin <ephemient@gmail.com>
2017-12-10 02:47:11 -0500

    Day 9: Unboxed arrays are faster

--- src/Day10.hs
+++ src/Day10.hs
@@ -5,37 +5,49 @@ Description:    <http://adventofcode.com/2017/day/10 Day 10: Knot Hash>
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day10 (day10a, day10b) where
 
+import Data.Array.IArray (IArray, (!), bounds, elems, ixmap, listArray)
+import Data.Array.Unboxed (UArray)
 import Data.Bits (xor)
 import Data.Bool (bool)
 import Data.Char (ord)
-import Data.List (foldl', replicate, unfoldr)
+import Data.Ix (Ix, inRange, index, rangeSize)
+import Data.List (foldl', replicate, scanl', unfoldr)
+import Data.Word (Word8)
 import Text.Printf (printf)
 
--- | @roll len list (count, skip)@ reverses the first @count@ elements of
--- @list@ then rolls both those reversed elements and an additional @skip@
--- elements from the start of the list to the end of the list.
---
--- @len@ must be equal to @length list@.
-roll :: Int -> [a] -> (Int, Int) -> [a]
-roll len list (count, skip) = end ++ start where
-    (knot, keep) = splitAt count list
-    (start, end) = splitAt skip $ keep ++ reverse knot
+-- | Reverse elements of an array in a range of indices. The range may wrap.
+reverseRange :: (IArray a e, Ix i, Num i) => a i e -> (i, i) -> a i e
+reverseRange arr r@(start, end) = ixmap b reverseIx arr where
+    b@(low, high) = bounds arr
+    ix = index b
+    reverseIx i
+      | start <= end, inRange r i = start + end - i
+      | start > end, inRange (start, high) i || inRange (low, end) i
+      = low + fromIntegral ((ix start + ix end - ix i) `mod` rangeSize b)
+      | otherwise = i
 
--- | @rolls len counts@ iterates @roll len list (count, skip)@, starting with
--- @list = [0..len-1]@, for each @count@ in @counts@ and @skip = 0, 1, ..@.
--- Finally, it unrotates by the total @count + skip@ sum.
-rolls :: Int -> [Int] -> [Int]
-rolls len counts = end ++ start where
-    list = [0 .. len - 1]
-    rotated = foldl (roll len) list . zip counts $ cycle list
-    rotation = sum (zipWith (+) [0..] counts) `mod` len
-    (start, end) = splitAt (len - rotation) rotated
+-- | Given array bounds and a list of lengths, returns a list of ranges in the
+-- array, with each one starting at an increasing distance from the end of the
+-- previous, wrapping around the ends of the array.
+knotRanges :: (Ix i, Num i) => (i, i) -> [Int] -> [(i, i)]
+knotRanges b@(low, high) counts =
+  [ (low + fromIntegral start, low + fromIntegral (addMod start $ len - 1))
+  | (len, start) <- zip counts $ scanl' addMod 0 $ zipWith (+) [0..] counts
+  , len > 0
+  ] where addMod x y = (x + y) `mod` rangeSize b
+
+-- | Sequentially reverses all ranges in an array from a list of lengths.
+hash :: (IArray a e, Ix i, Num i) => a i e -> [Int] -> a i e
+hash arr counts = foldl' reverseRange arr $ knotRanges (bounds arr) counts
 
 day10a :: Int -> String -> Int
-day10a len input = let x:y:_ = rolls len . read $ '[' : input ++ "]" in x * y
+day10a len input = hashed ! 0 * hashed ! 1 where
+    arr = listArray (0, len - 1) [0..] :: UArray Int Int
+    hashed = hash arr . read $ '[' : input ++ "]"
 
 day10b :: String -> String
-day10b input = map (foldl' xor 0) dense >>= printf "%02x" where
-    sparse = rolls 256 . concat . replicate 64 $
-        map ord (concat $ lines input) ++ [17, 31, 73, 47, 23]
-    dense = unfoldr (bool (Just . splitAt 16) (const Nothing) =<< null) sparse
+day10b input = dense >>= printf "%02x" . foldl' xor 0 where
+    arr = listArray (0, 255) [0..] :: UArray Int Word8
+    sparse = elems . hash arr . concat . replicate 64 $
+        (lines input >>= map ord) ++ [17, 31, 73, 47, 23]
+    dense = unfoldr (bool Nothing . Just . splitAt 16 <*> not . null) sparse
