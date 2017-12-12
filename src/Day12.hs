65cded9481ec4fa454b9769895f57ac9330247f2
Daniel Lin <ephemient@gmail.com>
2017-12-12 00:55:05 -0500

    Day 12: Shorten with fgl

--- src/Day12.hs
+++ src/Day12.hs
@@ -2,33 +2,22 @@
 Module:         Day12
 Description:    <http://adventofcode.com/2017/day/12 Day 12: Digital Plumber>
 -}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TupleSections, TypeApplications, ViewPatterns #-}
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day12 (day12a, day12b) where
 
+import Control.Arrow (second)
 import Data.Char (isDigit)
-import Data.List (unfoldr)
-import qualified Data.Map.Lazy as Map ((!?), fromList, keysSet)
-import Data.Map.Lazy (Map)
-import Data.Maybe (fromMaybe)
-import qualified Data.Set as Set ((\\), empty, insert, member, minView, size)
-import Data.Set (Set)
+import Data.Graph.Inductive (Gr, Graph, bfs, mkUGraph, noComponents)
 
-parse :: String -> Map Int [Int]
-parse = Map.fromList . map parseLine . lines where
-    parseLine (words -> x : "<->" : xs) = (read x, read . filter isDigit <$> xs)
-
-connected :: (Ord a) => Map a [a] -> a -> Set a
-connected neighbors = grow Set.empty . (:[]) where
-    grow s [] = s
-    grow s (x:xs) = grow (Set.insert x s) $ queue ++ xs where
-        queue = if Set.member x s then [] else fromMaybe [] $ neighbors Map.!? x
+-- | Creates an unlabeled graph with edges from each "0 <-> 1, 2.." line.
+parse :: (Graph gr) => String -> gr () ()
+parse = uncurry mkUGraph . second concat . unzip . map parseLine . lines where
+    parseLine (words -> (read -> x) : "<->" : xs) =
+        (x, (x,) . read . filter isDigit <$> xs)
 
 day12a :: String -> Int
-day12a = Set.size . flip connected 0 . parse
+day12a = length . bfs 0 . parse @Gr
 
 day12b :: String -> Int
-day12b input = length . unfoldr (fmap dropConnected . Set.minView) $
-               Map.keysSet neighbors where
-    neighbors = parse input
-    dropConnected (x, xs) = ((), xs Set.\\ connected neighbors x)
+day12b = noComponents . parse @Gr
