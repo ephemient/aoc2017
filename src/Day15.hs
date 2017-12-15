a8e30d23bf1c16bc9d5755a2bb51ad74510b8b46
Daniel Lin <ephemient@gmail.com>
2017-12-15 01:16:12 -0500

    Day 15: Improve performance by manual iteration (avoiding lists)

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,10 +2,13 @@
 Module:         Day15
 Description:    <http://adventofcode.com/2017/day/15 Day 15: Dueling Generators>
 -}
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day15 (day15a, day15b) where
 
+import Control.Arrow ((***))
 import Data.Bits ((.&.))
+import Data.Function (on)
 import Data.List (stripPrefix)
 
 -- | Returns the initial values for generators A and B.
@@ -15,16 +18,40 @@ parse input = (read a, read b) where
     Just a = stripPrefix "Generator A starts with " line1
     Just b = stripPrefix "Generator B starts with " line2
 
+-- | One step of generator A.
+genA :: Int -> Int
+genA x = x * 16807 `mod` 2147483647
+
+-- | One step of generator B.
+genB :: Int -> Int
+genB x = x * 48271 `mod` 2147483647
+
+-- | Step generator A until a multiple of 4.
+genA' :: Int -> Int
+genA' x = let y = genA x in if y .&. 3 == 0 then y else genA' y
+
+-- | Step generator A until a multiple of 8.
+genB' :: Int -> Int
+genB' x = let y = genB x in if y .&. 7 == 0 then y else genB' y
+
+-- | One step of both generators A and B.
+gen :: (Int, Int) -> (Int, Int)
+gen = genA *** genB
+
+-- | Step both generators A and B until a multiple of 4 and 8 respectively.
+gen' :: (Int, Int) -> (Int, Int)
+gen' = genA' *** genB'
+
+-- | prop> count p f x n == length (filter p . take n . tail $ iterate f x)
+count :: (a -> Bool) -> (a -> a) -> a -> Int -> Int
+count p f = count' 0 where
+    count' !k x 0 = k
+    count' !k x n = count' (if p y then k + 1 else k) y (n - 1) where y = f x
+
 day15a :: String -> Int
-day15a input = length . filter id . take 40000000 $ zipWith (==) a b where
-    (a0, b0) = parse input
-    a = map (.&. 0xffff) . tail $ iterate (\x -> x * 16807 `mod` 2147483647) a0
-    b = map (.&. 0xffff) . tail $ iterate (\x -> x * 48271 `mod` 2147483647) b0
+day15a input =
+    count (uncurry ((==) `on` (.&. 0xffff))) gen (parse input) 40000000
 
 day15b :: String -> Int
-day15b input = length . filter id . take 5000000 $ zipWith (==) a b where
-    (a0, b0) = parse input
-    a = map (.&. 0xffff) . filter ((== 0) . (.&. 3)) . tail $
-        iterate (\x -> x * 16807 `mod` 2147483647) a0
-    b = map (.&. 0xffff) . filter ((== 0) . (.&. 7)) . tail $
-        iterate (\x -> x * 48271 `mod` 2147483647) b0
+day15b input =
+    count (uncurry ((==) `on` (.&. 0xffff))) gen' (parse input) 5000000
