6a4bc163e702f815c9883a747c15b05233fd7e4c
Daniel Lin <ephemient@gmail.com>
2017-12-15 10:49:32 -0500

    Day 15: C-like speed with primitive types, unboxed tuples, and -fllvm

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,57 +2,57 @@
 Module:         Day15
 Description:    <http://adventofcode.com/2017/day/15 Day 15: Dueling Generators>
 -}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, MagicHash, UnboxedTuples #-}
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day15 (day15a, day15b) where
 
-import Control.Arrow ((***))
-import Data.Bits ((.&.))
-import Data.Function (on)
 import Data.List (stripPrefix)
-import Data.Word (Word64)
+import GHC.Prim (Word#, and#, eqWord#, remWord#, timesWord#)
+import GHC.Types (Int(I#), Word(W#))
 
 -- | Returns the initial values for generators A and B.
-parse :: String -> (Word64, Word64)
-parse input = (read a, read b) where
+parse :: String -> (# Word#, Word# #)
+parse input = (# a, b #) where
     [line1, line2] = lines input
-    Just a = stripPrefix "Generator A starts with " line1
-    Just b = stripPrefix "Generator B starts with " line2
+    Just (W# a) = read <$> stripPrefix "Generator A starts with " line1
+    Just (W# b) = read <$> stripPrefix "Generator B starts with " line2
 
 -- | One step of generator A.
-genA :: Word64 -> Word64
-genA x = x * 16807 `mod` 2147483647
+genA :: Word# -> Word#
+genA x = remWord# (timesWord# x 16807##) 2147483647##
 
 -- | One step of generator B.
-genB :: Word64 -> Word64
-genB x = x * 48271 `mod` 2147483647
-
--- | Step generator A until a multiple of 4.
-genA' :: Word64 -> Word64
-genA' x = let y = genA x in if y .&. 3 == 0 then y else genA' y
-
--- | Step generator A until a multiple of 8.
-genB' :: Word64 -> Word64
-genB' x = let y = genB x in if y .&. 7 == 0 then y else genB' y
+genB :: Word# -> Word#
+genB x = remWord# (timesWord# x 48271##) 2147483647##
 
 -- | One step of both generators A and B.
-gen :: (Word64, Word64) -> (Word64, Word64)
-gen = genA *** genB
+gen :: (# Word#, Word# #) -> (# Word#, Word# #)
+gen (# a, b #) = (# genA a, genB b #)
 
 -- | Step both generators A and B until a multiple of 4 and 8 respectively.
-gen' :: (Word64, Word64) -> (Word64, Word64)
-gen' = genA' *** genB'
+gen' :: (# Word#, Word# #) -> (# Word#, Word# #)
+gen' (# a, b #) = (# genA' a, genB' b #) where
+    genA' x = if W# (and# y 3##) == 0 then y else genA' y where y = genA x
+    genB' x = if W# (and# y 7##) == 0 then y else genB' y where y = genB x
 
--- | prop> count p f x n == length (filter p . take n . tail $ iterate f x)
-count :: (a -> Bool) -> (a -> a) -> a -> Int -> Int
-count p f = count' 0 where
-    count' !k x 0 = k
-    count' !k x n = count' (if p y then k + 1 else k) y (n - 1) where y = f x
+-- | Tests whether the lower 16 bits of each pair item are equal.
+eq16 :: (# Word#, Word# #) -> Bool
+eq16 (# a, b #) = I# (eqWord# (and# a 65535##) (and# b 65535##)) /= 0
+
+-- | @count n 0 x@ counts how many match 'eq' in
+-- @take n [gen x, gen (gen x), ..]@
+count :: Int -> Int -> (# Word#, Word# #) -> Int
+count 0 k _ = k
+count n !k x = count (n - 1) (if eq16 y then k + 1 else k) y where y = gen x
+
+-- | @count' n 0 x@ counts how many match 'eq' in
+-- @take n [gen x, gen (gen x), ..]@
+count' :: Int -> Int -> (# Word#, Word# #) -> Int
+count' 0 k _ = k
+count' n !k x = count' (n - 1) (if eq16 y then k + 1 else k) y where y = gen' x
 
 day15a :: String -> Int
-day15a input =
-    count (uncurry ((==) `on` (.&. 0xffff))) gen (parse input) 40000000
+day15a input = count 40000000 0 (parse input)
 
 day15b :: String -> Int
-day15b input =
-    count (uncurry ((==) `on` (.&. 0xffff))) gen' (parse input) 5000000
+day15b input = count' 5000000 0 (parse input)
