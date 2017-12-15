ef3f03a7cfc781bc2e124574034ac3c4ca8e45a2
Daniel Lin <ephemient@gmail.com>
2017-12-15 02:57:37 -0500

    Day 15: Unsigned Word64
    It's more correct on paltforms with 32-bit Int, and faster on platforms
    with 64-bit Int.

--- src/Day15.hs
+++ src/Day15.hs
@@ -10,36 +10,37 @@ import Control.Arrow ((***))
 import Data.Bits ((.&.))
 import Data.Function (on)
 import Data.List (stripPrefix)
+import Data.Word (Word64)
 
 -- | Returns the initial values for generators A and B.
-parse :: String -> (Int, Int)
+parse :: String -> (Word64, Word64)
 parse input = (read a, read b) where
     [line1, line2] = lines input
     Just a = stripPrefix "Generator A starts with " line1
     Just b = stripPrefix "Generator B starts with " line2
 
 -- | One step of generator A.
-genA :: Int -> Int
+genA :: Word64 -> Word64
 genA x = x * 16807 `mod` 2147483647
 
 -- | One step of generator B.
-genB :: Int -> Int
+genB :: Word64 -> Word64
 genB x = x * 48271 `mod` 2147483647
 
 -- | Step generator A until a multiple of 4.
-genA' :: Int -> Int
+genA' :: Word64 -> Word64
 genA' x = let y = genA x in if y .&. 3 == 0 then y else genA' y
 
 -- | Step generator A until a multiple of 8.
-genB' :: Int -> Int
+genB' :: Word64 -> Word64
 genB' x = let y = genB x in if y .&. 7 == 0 then y else genB' y
 
 -- | One step of both generators A and B.
-gen :: (Int, Int) -> (Int, Int)
+gen :: (Word64, Word64) -> (Word64, Word64)
 gen = genA *** genB
 
 -- | Step both generators A and B until a multiple of 4 and 8 respectively.
-gen' :: (Int, Int) -> (Int, Int)
+gen' :: (Word64, Word64) -> (Word64, Word64)
 gen' = genA' *** genB'
 
 -- | prop> count p f x n == length (filter p . take n . tail $ iterate f x)
