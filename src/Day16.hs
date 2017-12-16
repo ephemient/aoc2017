50d3df7b03a09dbb6949a07cbb822a2e4e817f9d
Daniel Lin <ephemient@gmail.com>
2017-12-16 15:17:00 -0500

    Day 16: Miscellaneous cleanups

--- src/Day16.hs
+++ src/Day16.hs
@@ -6,7 +6,6 @@ Description:    <http://adventofcode.com/2017/day/16 Day 16: Permutation Promena
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day16 (day16a, day16b) where
 
-import Control.Arrow (second)
 import Data.Array.IArray (IArray, Ix, (!), amap, bounds, elems, ixmap, listArray)
 import Data.Array.Unboxed (UArray)
 import Data.Bool (bool)
@@ -17,29 +16,31 @@ import Data.List (foldl', unfoldr)
 -- | A dance move.
 data Move i e = Spin i | Exchange i i | Partner e e
 
--- | Split words by whitespace or comma.
+-- | Split words by whitespaces or commas.
 words' :: String -> [String]
-words' = unfoldr $
-    bool Nothing . Just . second (drop 1) . break delim <*> not . null where
+words' = unfoldr $ bool Nothing . Just . breakDelim <*> not . all delim where
     delim c = c == ',' || isSpace c
+    breakDelim = break delim . dropWhile delim
 
 -- | Parse a list of dance moves.
 parse :: String -> [Move Int Char]
 parse = map parseMove . words' where
-    parseMove ('s':d) = Spin $ read d
-    parseMove ('x':(break (== '/') -> (x, _:y))) = Exchange (read x) (read y)
-    parseMove ['p',a,'/',b] = Partner a b
+    parseMove ('s' : d) = Spin $ read d
+    parseMove ('x' : (break (== '/') -> (x, _:y))) = Exchange (read x) (read y)
+    parseMove ['p', a, '/', b] = Partner a b
 
 -- | Compose two permutations.
 (-*-) :: (IArray a e, IArray a' i, Ix i) => a i e -> a' i i -> a i e
 a -*- b = ixmap (bounds a) (b !) a
+infixl 7 -*-
 
--- | Exponentiate a permutation.
+-- | Repeat a permutation.
 (-^-) :: (IArray a i, Ix i) => a i i -> Int -> a i i
 a -^- 0 = listArray <*> range $ bounds a
 a -^- n
   | (h, 0) <- n `divMod` 2 = (a -*- a) -^- h
   | (h, 1) <- n `divMod` 2 = (a -*- a) -^- h -*- a
+infixr 8 -^-
 
 -- | Like 'id', but swapping the two given values.
 exchange :: (Eq a) => a -> a -> a -> a
@@ -67,8 +68,7 @@ day16a :: Int -> String -> String
 day16a = flip day16b 1
 
 day16b :: Int -> Int -> String -> String
-day16b size n input =
-    let dance = parse input
-        pn = permuteNames size dance -^- n :: UArray Char Char
-        pi = permuteIndices size dance -^- n :: UArray Int Int
-    in map (elems pn !!) (elems pi)
+day16b size n input = map (elems pn !!) (elems pi) where
+    dance = parse input
+    pn = permuteNames size dance -^- n :: UArray Char Char
+    pi = permuteIndices size dance -^- n :: UArray Int Int
