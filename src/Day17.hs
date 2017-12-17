5b0582795116271076e73b4f5b01656a40bff6b5
Daniel Lin <ephemient@gmail.com>
2017-12-17 14:34:38 -0500

    Day 17: Count by len/step instead of by 1
    This reduces the iterations to O(log(top)/log(step)) from O(top).

--- src/Day17.hs
+++ src/Day17.hs
@@ -17,6 +17,10 @@ day17a step = Seq.index s $ (i + 1) `mod` Seq.length s where
         pos' = (pos + step) `mod` Seq.length s + 1
 
 day17b :: Int -> Int
-day17b step = snd $ foldl' insert (0, 0) [1..50000000] where
-    insert (pos, !next) len = (pos', bool next len $ pos' == 1)
-      where pos' = (pos + step) `mod` len + 1
+day17b step = insert 0 1 0 where
+    insert pos len !next
+      | len' > 50000000 = next
+      | otherwise = insert pos' len' . bool next (len + q) $ pos' == 1 where
+        q = (len - pos) `div` (step + 1)
+        pos' = (pos + (q + 1) * (step + 1) - 1) `mod` (len + q) + 1
+        len' = len + q + 1
