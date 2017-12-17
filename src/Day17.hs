9015826631124e23cdc2983b7c4c0e9f978dfb38
Daniel Lin <ephemient@gmail.com>
2017-12-17 01:41:59 -0500

    Day 17: Strictness for 30x speed

--- src/Day17.hs
+++ src/Day17.hs
@@ -2,6 +2,7 @@
 Module:         Day17
 Description:    <http://adventofcode.com/2017/day/17 Day 17: Spinlock>
 -}
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day17 (day17a, day17b) where
 
@@ -17,5 +18,5 @@ day17a step = Seq.index s $ (i + 1) `mod` Seq.length s where
 
 day17b :: Int -> Int
 day17b step = snd $ foldl' insert (0, 0) [1..50000000] where
-    insert (pos, next) len = (pos', bool next len $ pos' == 1)
+    insert (pos, !next) len = (pos', bool next len $ pos' == 1)
       where pos' = (pos + step) `mod` len + 1
--- test/Day17Spec.hs
+++ test/Day17Spec.hs
@@ -4,7 +4,10 @@ import Day17 (day17a, day17b)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec =
+spec = do
     describe "part 1" $
         it "examples" $
             day17a 3 `shouldBe` 638
+    describe "part 2" $
+        it "examples" $
+            day17b 3 `shouldBe` 1222153
