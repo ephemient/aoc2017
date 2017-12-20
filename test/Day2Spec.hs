8471387dffe032bb3704cb37dcfb2aa6c8165c0b
Daniel Lin <ephemient@gmail.com>
2017-12-20 15:15:31 -0500

    Revert "Day 2: Lift out test example"
    This reverts commit 5afeac90f4dee18305f825f36bb480f6bc4afd8f.

--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -3,18 +3,19 @@ module Day2Spec (spec) where
 import Day2 (day2a, day2b)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
-example :: String
-example = unlines
-  [ "5 1 9 5"
-  , "7 5 3"
-  , "2 4 6 8"
-  ]
-
 spec :: Spec
 spec = do
     describe "part 1" $
         it "examples" $
-            day2a example `shouldBe` 18
+            day2a (unlines
+              [ "5 1 9 5"
+              , "7 5 3"
+              , "2 4 6 8"
+              ]) `shouldBe` 18
     describe "part 2" $
         it "examples" $
-            day2b example `shouldBe` 9
+            day2b (unlines
+              [ "5 9 2 8"
+              , "9 4 7 3"
+              , "3 8 6 5"
+              ]) `shouldBe` 9
