9b67f917e69003f06a3a2019307ca1e4be3921e9
Daniel Lin <ephemient@gmail.com>
2017-12-24 03:54:17 -0500

    Day 21: Massive speedup by using a f^3(3x3)=9*3x3 lookup table
    Inspired by u/PrimesAreMyFavorite on Reddit.
    https://www.reddit.com/comments/7l78eb/-/drk8j2m

--- src/Day21.hs
+++ src/Day21.hs
@@ -2,12 +2,13 @@
 Module:         Day21
 Description:    <http://adventofcode.com/2017/day/21 Day 21: Fractal Art>
 -}
+{-# LANGUAGE TupleSections, ViewPatterns #-}
 {-# OPTIONS_HADDOCK ignore-exports #-}
 module Day21 (day21, day21a, day21b) where
 
-import Data.List (transpose)
+import Data.List (foldl', transpose)
 import Data.List.Split (chunksOf, splitOn)
-import qualified Data.Map.Lazy as Map ((!), fromList)
+import qualified Data.Map.Lazy as Map ((!), assocs, empty, fromList, fromListWith, insert, map, member, unionsWith)
 import Data.Map.Lazy (Map)
 
 -- | 8 affine transformations.
@@ -38,11 +39,32 @@ step rules grid = assemble $ map (rules Map.!) exploded where
     exploded = chunksOf d grid >>= transpose . map (chunksOf d)
     assemble parts = chunksOf n parts >>= map concat . transpose
 
+-- | Returns the same valeu for all transformations of the same block.
+canonicalize :: (Ord a) => [[a]] -> [[a]]
+canonicalize grid = minimum [t grid | t <- transforms]
+
+-- | Returns the number of occurrences of each type of 3x3 block, starting from
+-- a 3x3 block, after iterating the rules @3*n@ times.
+--step3 :: (Ord a) => Map [[a]] [[a]] -> [[a]] -> Int -> [([[a]], Int)]
+step3 rules (canonicalize -> grid) n = iterate expand3 [(grid, 1)] !! n where
+    rules3 = step' Map.empty grid
+    step' expansions grid
+      | Map.member grid expansions = expansions
+      | otherwise = foldl' step' (Map.insert grid counts expansions) parts
+      where
+        parts = chunksOf 3 (iterate (step rules) grid !! 3) >>=
+                map canonicalize . transpose . map (chunksOf 3)
+        counts = Map.fromListWith (+) $ (, 1) <$> parts
+    expand3 counts = Map.assocs $ Map.unionsWith (+)
+        [Map.map (* count) $ rules3 Map.! grid | (grid, count) <- counts]
+
 -- | @day21 n input@ returns the number of bits set in the @n@-th 'step' of
 -- transforming the 'start' glider using 'parse' rules from @input@.
 day21 :: Int -> String -> Int
-day21 n input =
-    length . filter id . concat $ iterate (step $ parse input) start !! n
+day21 n (parse -> rules) = sum
+  [ count * length (iterate (step rules) grid !! r >>= filter id)
+  | (grid, count) <- step3 rules start q
+  ] where (q, r) = n `quotRem` 3
 
 day21a :: String -> Int
 day21a = day21 5
